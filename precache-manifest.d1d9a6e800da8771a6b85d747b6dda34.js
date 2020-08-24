3fd3e7eb3f26e6b856144f3ee42ced430a279f37
System Administrator <root@MacBook-Pro-de-Vitor.local>
2020-08-24 19:26:11 -0300

    Updates

---
+++ precache-manifest.d1d9a6e800da8771a6b85d747b6dda34.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3656c9132f324404743c177a3d6a50d4",
+    "url": "/atelie-teste/index.html"
+  },
+  {
+    "revision": "bc7e96a2c1b08f3f933f",
+    "url": "/atelie-teste/static/css/main.d9c4c82f.chunk.css"
+  },
+  {
+    "revision": "560c2e4ebb5f8946f64f",
+    "url": "/atelie-teste/static/js/2.ba217aa3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/atelie-teste/static/js/2.ba217aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc7e96a2c1b08f3f933f",
+    "url": "/atelie-teste/static/js/main.6a7b184b.chunk.js"
+  },
+  {
+    "revision": "f40f7de807b674caaa92",
+    "url": "/atelie-teste/static/js/runtime-main.e776edb2.js"
+  },
+  {
+    "revision": "c1896640f8dcbdb127ef3fcb1324b6d2",
+    "url": "/atelie-teste/static/media/background-marrom.c1896640.png"
+  },
+  {
+    "revision": "b5458aa4878ad03334beccf39506eea0",
+    "url": "/atelie-teste/static/media/footer.b5458aa4.png"
+  },
+  {
+    "revision": "ed31afbce0fa210524f9c2f2ffd43992",
+    "url": "/atelie-teste/static/media/header.ed31afbc.png"
+  }
+]);
